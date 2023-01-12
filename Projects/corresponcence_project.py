75d9cf7d08d3c91a5b30bd95ac3afe7af193f44f
Luis Henrique Carvalho Araujo <luishenri60@gmail.com>
2023-01-12 13:29:51 -0300

    project completed

--- Projects/corresponcence_project.py
+++ Projects/corresponcence_project.py
@@ -22,10 +22,11 @@ This message has an offset of 10. Can you decode it?
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 mensage = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
 
+
 def caesar_decode(code, offset):
     decoded_mensage = ""
     for letter in code:
-        if not(letter in alphabet):
+        if not (letter in alphabet):
             decoded_mensage += letter
         else:
             letter_position = alphabet.find(letter)
@@ -34,10 +35,11 @@ def caesar_decode(code, offset):
             decoded_mensage += decoded_letter
     return decoded_mensage
 
+
 def caesar_encode(code, offset):
     decoded_mensage = ""
     for letter in code:
-        if not(letter in alphabet):
+        if not (letter in alphabet):
             decoded_mensage += letter
         else:
             letter_position = alphabet.find(letter)
@@ -45,6 +47,7 @@ def caesar_encode(code, offset):
             decoded_letter = alphabet[lp_new]
             decoded_mensage += decoded_letter
     return decoded_mensage
+
 
 my_message = "hello, are you ok?"
 
@@ -86,7 +89,7 @@ Good luck!
 mensage3 = "vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx."
 
 for i in range(len(alphabet)):
-    print(str(i + 1) +" "+ caesar_decode(mensage3, i+1))
+    print(str(i + 1) + " " + caesar_decode(mensage3, i+1))
 
 '''
 Salutations! As you can see, technology has made brute forcing simple ciphers
@@ -124,35 +127,32 @@ eoxum ov hnh gvb
 As you can imagine, this is a lot harder to crack without knowing the keyword!
 So now comes the hard part. I'll give you a message and the keyword, and you'll
 see if you can figure out how to crack it! Ready? Okay here's my message:
-txm srom vkda gl lzlgzr qpdb? fepb ejac! ubr imn tapludwy mhfbz cza ruxzal
-wg dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!
+dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!
 and the keyword to decode my message is 
 friends
 Because that's what we are! Good luck friend!
 '''
 
-key = 'friends'
-mensage4 = 'dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!'
 
 def vigenere_decode(code, key):
     new_key = ''
-    x = 0
     code_words = code.split()
     key_words = []
     n_code_words = []
     n_key_words = []
     final_numbers = []
+    decoded_mensage_raw = ''
     decoded_mensage = ''
     trash = ''
+    x = 0
     z = 0
-    y = 0
     for word in code_words:
         new_key = ''
         code_letter_position = ''
-        
+
         for letter in word:
-            if not(letter in alphabet):
-              trash += letter
+            if not (letter in alphabet):
+                trash += letter
             else:
                 if x < len(key):
                     new_key += key[x]
@@ -164,20 +164,79 @@ def vigenere_decode(code, key):
                 code_letter_position = str(alphabet.find(letter))
                 n_code_words.append(code_letter_position)
         key_words.append(new_key)
-    
-    for i in  range(0,len(n_code_words)):
+
+    for i in range(0, len(n_code_words)):
         final_numbers.append(int(n_code_words[i]) - int(n_key_words[i]))
-    
+
     for number in final_numbers:
+        decoded_mensage_raw += alphabet[number]
 
-        decoded_mensage += alphabet[number]
+    for letter in code:
+        if not (letter in alphabet):
+            decoded_mensage += letter
+        else:
+            decoded_mensage += decoded_mensage_raw[z]
+            z += 1
+    return decoded_mensage
 
-        if len(code_words[z])  == len(decoded_mensage) - y:
-            decoded_mensage += ' '
-            y = len(decoded_mensage)
-            z += 1            
-            
-            
-    return key_words, n_key_words, code_words, n_code_words, decoded_mensage, final_numbers
+
+def vigenere_encode(code, key):
+    new_key = ''
+    code_words = code.split()
+    key_words = []
+    n_code_words = []
+    n_key_words = []
+    final_numbers = []
+    decoded_mensage_raw = ''
+    decoded_mensage = ''
+    trash = ''
+    x = 0
+    z = 0
+    for word in code_words:
+        new_key = ''
+        code_letter_position = ''
+
+        for letter in word:
+            if not (letter in alphabet):
+                trash += letter
+            else:
+                if x < len(key):
+                    new_key += key[x]
+                    key_letter_position = str(alphabet.find(key[x]))
+                    n_key_words.append(key_letter_position)
+                    x += 1
+                    if x >= len(key):
+                        x = 0
+                code_letter_position = str(alphabet.find(letter))
+                n_code_words.append(code_letter_position)
+        key_words.append(new_key)
+
+    for i in range(0, len(n_code_words)):
+            x = 0
+            x = int(n_code_words[i]) + int(n_key_words[i])
+            if x > 25:
+                x -= 26
+                final_numbers.append(x)
+            else:
+                final_numbers.append(x)
+
+    for number in final_numbers:
+        decoded_mensage_raw += alphabet[number]
+
+    for letter in code:
+        if not (letter in alphabet):
+            decoded_mensage += letter
+        else:
+            decoded_mensage += decoded_mensage_raw[z]
+            z += 1
+    return decoded_mensage
+
+
+key = 'friends'
+mensage4 = 'dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!'
+
+key1 = 'friends'
+mensage5 = 'you were able to decode this? nice work! you are becoming quite the expert at crytography!'
 
 print(vigenere_decode(mensage4, key))
+print(vigenere_encode(mensage5, key1))
