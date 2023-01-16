0f9658c460d1cb2d34aaf4c3689701842953c8e1
Luis Henrique Carvalho Araujo <luishenri60@gmail.com>
2023-01-16 15:34:37 -0300

    Update us-medical-insurance-costs.py

--- us-medical-insurance-costs/us-medical-insurance-costs.py
+++ us-medical-insurance-costs/us-medical-insurance-costs.py
@@ -1,7 +1,8 @@
 import csv
 
-with open("insurance.csv", "r") as insurance_csv_file:
+with open('codecademy_data_analyst/us-medical-insurance-costs/insurance.csv', 'r') as insurance_csv_file:
     csv_reader = csv.reader(insurance_csv_file)
 
     for row in csv_reader:
         print(row)
+        
